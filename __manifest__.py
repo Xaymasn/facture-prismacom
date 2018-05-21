eab9db0abe87d65c1138a2241fa28cb0b43817e1
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-21 13:34:35 +0000

    Suppression des fichiers inutiles

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Personalisation Facture Prismacom",
-
     'summary': "Juste pour personnaliser les factures de prismacom afin de satisfaire à leur exigences.",
-
     'description': " ",
-
     'author': "Elhadji Malang Diedhiou",
     'website': "http://www.supermalang.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Sales',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'version': '11.0.0.1',
+    'depends': ['sale'],
     'data': [
         'reports/prismacom_order_report.xml',
         'reports/prismacom_order_template.xml'
