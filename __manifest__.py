3bfafb52d7c42c27296a4fe9510bcab1544c2444
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2018-05-21 15:52:38 +0000

    Corrections

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'version': '11.0.0.1',
     'depends': ['sale_management'],
     'data': [
-        'reports/rtp_order_report.xml',
-        'reports/rtp_order_template.xml'
+        'reports/rtp_sale_order_report.xml',
+        'reports/rtp_sale_order_template.xml'
     ]
 }
